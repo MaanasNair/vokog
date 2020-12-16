465b553dfd864eee8697e83c8e55c86887b95ef9
MaanasNair <bee.birdiecorporation@gmail.com>
2020-12-16 13:27:54 +0530

    update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+/*
+this repo is and will be maintained by Maanas Nair @MaanasNair (github.com/MaanasNair)(MaanasNair.in)
+*/
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
