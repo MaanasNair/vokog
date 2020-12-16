426b9afeb15d36a800f538fae4d69aad5025a231
MaanasNair <bee.birdiecorporation@gmail.com>
2020-12-16 15:51:15 +0530

    Update Email Verification

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Defaults
+    | Authentication Defaults (added email and fixed email verification)
     |--------------------------------------------------------------------------
     |
     | This option controls the default authentication "guard" and password
