c4b2f23f3dfad022c2632fa8cd0cdb3aabdc6328
MaanasNair <bee.birdiecorporation@gmail.com>
2020-12-17 16:48:28 +0530

    Bugs removed and changed from alpha to beta

--- index.php
+++ index.php
@@ -48,9 +48,6 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-/*
-This repo is and will be maintained by Maanas Nair @MaanasNair (github.com/MaanasNair)(MaanasNair.in)
-*/
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
