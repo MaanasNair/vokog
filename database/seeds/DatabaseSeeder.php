ab4eed494d97191af8c2bcaa157d06bdf2b4e81b
MaanasNair <bee.birdiecorporation@gmail.com>
2020-12-17 17:11:14 +0530

    Update Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.(Made by Maanas Nair)
+     * Seed the application's database.
      *
      * @return void
      */
